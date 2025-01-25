efa3bae6c3d3821b23c3d3d72bb964f906511f06
jmd246 <jdoucette69.jd@gmail.com>
2025-01-25 14:09:56 -0500

    Connected to library fecthed author names

--- library/src/main/java/library/Main.java
+++ library/src/main/java/library/Main.java
@@ -1,7 +1,38 @@
 package library;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // Database URL, username, and password
+       String url = "jdbc:mysql://localhost:3306/library";
+       String user = "root";
+       String password = "";
+       // try to open connection
+       try (Connection connection = DriverManager.getConnection(url, user, password)) {
+          System.out.println("Connected to the database successfully!");
+          // Create a Statement object
+          Statement statement = connection.createStatement();
+
+          // Execute a query
+          String query = "SELECT * FROM author";
+          ResultSet resultSet = statement.executeQuery(query);
+
+          // Process the result set
+          while (resultSet.next()) {
+            System.out.println("Author Name: " + resultSet.getString("Author_name"));
+          }
+
+
+        } 
+        catch (SQLException e) {
+           System.out.println("Failed to connect to the database.");
+           e.printStackTrace();
+        }
     }
 }
