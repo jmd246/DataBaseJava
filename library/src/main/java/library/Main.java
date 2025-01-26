d5c0f88feee927f95180685a6b0b88f9d869e09b
jmd246 <jdoucette69.jd@gmail.com>
2025-01-26 16:16:00 -0500

    Fetch and update with prepared statements

--- library/src/main/java/library/Main.java
+++ library/src/main/java/library/Main.java
@@ -2,12 +2,38 @@ package library;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 
 public class Main {
+   private static void fetchAuthors(Connection connection) throws SQLException{
+      // Execute a query
+      String fetchQuery = "SELECT * FROM author ORDER BY Author_name";
+      try(PreparedStatement preparedStatement = connection.prepareStatement(fetchQuery)){
+         ResultSet resultSet = preparedStatement.executeQuery();
+         // Process the result set  fetching author table which has only author name
+         while (resultSet.next()) {
+            System.out.println("Author Name: " + resultSet.getString("Author_name"));
+         }
+      }
+   }
+   public static void updateAuthor(Connection connection, String oldName, String newName) throws SQLException {
+      String updateQuery = "UPDATE author SET Author_name = ? WHERE Author_name = ?" ;
+      try (PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)){
+         preparedStatement.setString(1, newName);
+         preparedStatement.setString(2, oldName);
+
+         int rowsUpdated = preparedStatement.executeUpdate();
+         if (rowsUpdated > 0) {
+            System.out.println("Author updated successfully.");
+         }
+         else { 
+            throw new SQLException("UPDATE FAILED, NO AUTHOR FOUND WITH THE NAME: "+oldName);
+         }
+      }
+  }
     public static void main(String[] args) {
        // Database URL, username, and password
        String url = "jdbc:mysql://localhost:3306/library";
@@ -15,20 +41,20 @@ public class Main {
        String password = "";
        // try to open connection
        try (Connection connection = DriverManager.getConnection(url, user, password)) {
-          System.out.println("Connected to the database successfully!");
-          // Create a Statement object
-          Statement statement = connection.createStatement();
+          
+         System.out.println("Connected to the database successfully!");
+         // Create a Statement object
+         //read
+         fetchAuthors(connection);
+          //update
+         updateAuthor(connection, "Goku", "Vegeta");
+         fetchAuthors(connection);
 
-          // Execute a query
-          String query = "SELECT * FROM author";
-          ResultSet resultSet = statement.executeQuery(query);
-
-          // Process the result set
-          while (resultSet.next()) {
-            System.out.println("Author Name: " + resultSet.getString("Author_name"));
-          }
-
-
+          //delete
+          //create
+          //join
+          //inner join
+          //outer join
         } 
         catch (SQLException e) {
            System.out.println("Failed to connect to the database.");
